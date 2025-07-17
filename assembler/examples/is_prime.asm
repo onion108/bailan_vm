db04aa77d7e5d56bceebedba448ae12deb636705
27Onion Nebell <zzy20080201@gmail.com>
2025-07-17 18:08:52 +0800

    feat: complete example `is_prime`

--- assembler/examples/is_prime.asm
+++ assembler/examples/is_prime.asm
@@ -8,14 +8,26 @@
 @p _main
 intr 0x01
 mov_ans r0
-mov 2, r1
+mov 0, r7
+cmp r0, r7, 0x00
+jmp_cond is_not_prime
 mov 1, r7
 cmp r0, r7, 0x00
 jmp_cond is_not_prime
-jmp calc_loop
+mov 2, r7
 
 ; Calculation loop
 @p calc_loop
+mod r0, r7
+mov_ans r1
+mov 0, r8
+cmp r1, r8, 0x00
+jmp_cond is_not_prime
+mov 1, r8
+add r7, r8
+mov_ans r7
+cmp r7, r0, 0x03
+jmp_cond calc_loop
 
 
 @p is_prime
